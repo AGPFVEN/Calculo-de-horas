ba58ab275c921e6a41b080403b728fbbf89ece91
Alfonso Pineda <50179663+AGPFVEN@users.noreply.github.com>
2024-10-24 11:19:54 +0200

    todo

--- calcular_día.ps1
+++ calcular_día.ps1
@@ -1,7 +1,14 @@
 param($p1)
+
+#Componer fecha
 if ($p1){
       $mydate = $p1
 } else {
       $mydate = -join($(Get-Date -Format "dd-MM-yyyy"), ".", "txt")
 }
-python .\timem.py $mydate
+
+if (Test-Path $mydate -PathType Leaf){
+      python .\timem.py $mydate
+} else {
+      New-Item -Name $mydate -ItemType "file"
+}
